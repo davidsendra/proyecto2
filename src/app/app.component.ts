73f4645eba752cb03a8aa94a3a244ba934754df2
fito <amiguitoandroide@gmail.com>
2022-08-27 18:41:45 -0300

    otro branch2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,7 +9,7 @@ export class AppComponent {
   nombre = 'Rodriguez Pablo';
   edad = 40;
   email = 'rpablo@gmail.com';
-  sueldos = [1700, 195000, 1900];
+  sueldos = [1700, 195000, 2000];
   activo = true;
 
   esActivo() {
